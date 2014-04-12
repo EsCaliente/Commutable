e9cb5b7f8782b750600bca01b6b44b37e07c0f76
EsCaliente <e.damisch42@gmail.com>
2014-04-12 16:40:07 -0500

    Added Navigation and Commute Creation Layout
    Added Navigation capability to Commute Creation screen, and started
    playing with the basic layout

---
+++ Commutable/Commutable/CommuteCreationScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  CommuteCreationScreenViewController.h
+//  Commutable
+//
+//  Created by Edward Damisch on 4/12/14.
+//  Copyright (c) 2014 Commutable. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommuteCreationScreenViewController : UIViewController
+
+@end
